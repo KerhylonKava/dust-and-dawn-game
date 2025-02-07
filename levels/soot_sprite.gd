5a3e896c780e71a6a519bdb561d2adb320812f7c
Kava <esthercward1@gmail.com>
2025-02-06 17:42:51 -0800

    Jumps fixed and double jump implemented

--- levels/soot_sprite.gd
+++ levels/soot_sprite.gd
@@ -1,7 +1,16 @@
 extends CharacterBody2D
+const UP_DIRECITON := Vector2.UP
 
 @export var speed := 750
-var jumpTime = 0
+
+var jump_time = 0
+var jump_strength := 1000
+var max_jumps := 2
+var jumps_made := 0
+
+var is_falling := velocity.y > 0 and not is_on_floor()
+
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,15 +20,19 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	#Movement
-	if jumpTime > 0:
-		jumpTime -= 1
-		print(jumpTime)
+	if jump_time > 0:
+		jump_time -= 1
+		print(jump_time)
 	
 	if Input.is_action_just_pressed("up"):
 		#if collisionShape2D:
-		if jumpTime == 0:
-			jumpTime = 15
-		print(jumpTime)
+		if is_on_floor():
+			jump_time = 15
+			jumps_made = 1
+		elif jumps_made < max_jumps:
+			jump_time += 5
+			jumps_made += 1
+		print(jump_time,",", jumps_made)
 	
 	if Input.is_action_pressed("left"):
 		velocity.x = -speed
@@ -33,11 +46,12 @@ func _process(delta: float) -> void:
 		if velocity.x < 0:
 			velocity.x -= 0.15 * velocity.x
 			
-	if jumpTime > 0:
-		velocity.y = -speed
+	if jump_time > 0:
+		velocity.y = -jump_strength
 	
 	#Gravity
-	velocity.y += 50
+	velocity.y += 60
 
 	move_and_slide()
+	
 	

a5205e57711b7df67447e319120f749449fb0b04
Kava <esthercward1@gmail.com>
2025-02-08 19:34:38 -0800

    quick not-fixes

--- inventory/scripts/inv_ui.gd
+++ inventory/scripts/inv_ui.gd
@@ -1,17 +1,9 @@
 extends Control
 
-@onready var inventory : Inventory = preload("res://inventory/player_inv.tres")
-@onready var slots: Array = $NinePatchRect/GridContainer.get_children()
-
 var is_open = false
 
 func _ready():
-	update_slots()
 	close()
-
-func update_slots():
-	for i in range (min(inventory.items.size(), slots.size())):
-		slots[i].update(inventory.items[i])
 
 func _process(delta):
 	if Input.is_action_just_pressed("inventory"):

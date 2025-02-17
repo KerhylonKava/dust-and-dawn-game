877e653f4712aaa38574d72375b1653c7fa00c8f
Kava <esthercward1@gmail.com>
2025-02-17 10:58:09 -0800

    Parallax background working

--- test.gd
+++ test.gd
@@ -32,3 +32,6 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 func _on_area_2d_body_exited(body: Node2D) -> void:
 	if body.has_method("soot_sprite"):
 		player_in_area = false
+		
+func light_lantern():
+	pass

c0a45016133cec2f493871082158dccb41e0ff27
Kava <esthercward1@gmail.com>
2025-02-17 18:36:14 -0800

    Main Menu!

--- inventory/scripts/collectable.gd
+++ inventory/scripts/collectable.gd
@@ -8,8 +8,8 @@ var SootSprite = null
 func _on_interactable_area_body_entered(body: Node2D) -> void:
 	if body.has_method("soot_sprite"):
 		SootSprite = body
-		playercollect()
 		visible = false
+		playercollect()
 
 func playercollect():
 	SootSprite.collect(item)

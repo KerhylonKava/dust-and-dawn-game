d5b4da14cad4cdc31da4f6c9f08b1fddf7a86c76
Luca S <lucaperezsalloum@gmail.com>
2025-03-29 13:03:06 -0700

    can grown plants with water

--- levels/3level/aging_component.gd
+++ levels/3level/aging_component.gd
@@ -4,7 +4,7 @@ extends Node
 signal age_changed(new_age : float, last_age : float) 
 signal age_threshold_reached(new_scene : Node2D)
 
-@export var target = self
+@export var target : Node2D
 @export var current_age = 0.0:
 	set(value):
 		if(current_age != value):
@@ -29,7 +29,7 @@ static var group_name = "AgingComponent"
 
 func _ready():
 	if(target == null):
-		target.get_parent()
+		target = get_parent()
 		
 	add_to_group(group_name)
 
@@ -37,6 +37,6 @@ var _threshold_reached = false
 
 func _create_next_scene() -> Node2D:
 	var instance : Node2D = next_scene.instantiate()
-	self.get_parent().add_child(instance)
+	target.get_parent().add_child(instance)
 	instance.global_transform = target.global_transform
 	return instance

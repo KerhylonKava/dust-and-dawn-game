a494f7fb2b802dcd2eccace2f094ab97262a4f95
Kava <154646695+KerhylonKava@users.noreply.github.com>
2025-04-01 21:44:58 -0700

    Item Bug Fixes

--- levels/3level/water_collectable.gd
+++ levels/3level/water_collectable.gd
@@ -1,19 +1,18 @@
 extends StaticBody2D
 
-@export var leaf: InvItem
+@export var water: InvItem
 var SootSprite = null
 
 func _on_interactable_area_body_entered(body: Node2D) -> void:
 	if body.has_method("soot_sprite"):
 		SootSprite = body
-		playercollect(leaf,1)
+		playercollect(water,1)
 		queue_free()
 		#visible = false
 
 func playercollect(item,amount):
 	# Collect the item and trigger a signal
 	SootSprite.collect(item,amount)
-	print(item.name,amount)
 	
 	# Set a flag to indicate that the string sprite has been collected
 	#SootSprite.has_collected_string_sprite = true

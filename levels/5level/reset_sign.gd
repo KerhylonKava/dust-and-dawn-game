b33d0d139ba5a4ebc128fea67295cbe1185fd642
Luca S <lucaperezsalloum@gmail.com>
2025-07-24 13:53:36 -0700

    Respawn for crates

---
+++ levels/5level/reset_sign.gd
@@ -0,0 +1,43 @@
+extends Node2D
+
+var crate_scene = preload("res://levels/5level/crate.tscn") 
+var SootSprite = null
+var player_in_area = false
+@onready var crate2 = $Crate2
+@onready var crate3 = $Crate3
+@onready var crate4 = $Crate4
+
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if body.has_method("soot_sprite"):
+		player_in_area = false
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.has_method("soot_sprite"):
+		player_in_area = true
+		SootSprite = body
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	crate2.global_position = Vector2(2372, -177) 
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if player_in_area:
+		$space.visible = true
+	elif !player_in_area:
+		$space.visible = false
+
+	
+	if Input.is_action_just_pressed("space") and player_in_area:
+		remove_child(crate2)
+		add_child(crate2)
+		crate2.global_position = Vector2(2372, -177)
+		remove_child(crate3)
+		add_child(crate3)
+		crate3.global_position = Vector2(2372,-292)
+		remove_child(crate4)
+		add_child(crate4)
+		crate4.global_position = Vector2(2372, -409)
--- levels/soot_sprite/soot_sprite.gd
+++ levels/soot_sprite/soot_sprite.gd
@@ -108,11 +108,11 @@ func _process(delta: float) -> void:
 
 
 	for i in get_slide_collision_count():
+		print("Moving")
 		var collision = get_slide_collision(i)
 		var collision_crate = collision.get_collider()
 		if collision_crate.is_in_group("Crates") and abs(collision_crate.get_linear_velocity().x) < MAX_VELOCITY: 
 			collision_crate.apply_central_impulse(collision.get_normal() * -PUSH_FORCE)
-			print("Moving")
 	move_and_slide()
 	
  

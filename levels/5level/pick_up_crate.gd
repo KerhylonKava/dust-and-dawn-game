96f4b627d36d4b4f40811cc5113201d4f092f6f1
Luca S <lucaperezsalloum@gmail.com>
2025-07-28 16:57:04 -0700

    fixed issue with collision layers.
    Need to: Fix ability to throw block

--- levels/5level/pick_up_crate.gd
+++ levels/5level/pick_up_crate.gd
@@ -11,16 +11,16 @@ func _input(event: InputEvent) -> void:
 		var bodies = $Area2D.get_overlapping_bodies() 
 		for body in bodies:
 			if body.name == "SootSprite" and get_node("../SootSprite").canPick == true:
-				self.freeze = true
-				self.freeze_mode = RigidBody2D.FREEZE_MODE_STATIC #Check if need to remove freeze
+				#self.freeze = true
+				#self.freeze_mode = RigidBody2D.FREEZE_MODE_STATIC #Check if need to remove freeze
 				picked = true
 				get_node("../SootSprite").canPick = false
 				
 	if Input.is_action_just_pressed("drop") and picked == true:
 		picked = false
 		get_node("../SootSprite").canPick = true
-		self.freeze = true
-		self.freeze_mode = RigidBody2D.FREEZE_MODE_STATIC
+		#self.freeze = true
+		#self.freeze_mode = RigidBody2D.FREEZE_MODE_STATIC
 		if get_node("../SootSprite/SootSpriteImage").flip_h == false: 
 			apply_impulse(Vector2(), Vector2(90, - 10)) 
 		else:
@@ -29,8 +29,9 @@ func _input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("throw") and picked == true: 
 		picked = false
 		get_node("../SootSprite").canPick = true
-		self.freeze = true
-		self.freeze_mode = RigidBody2D.FREEZE_MODE_STATIC
+		picked = false
+		#self.freeze = true
+		#self.freeze_mode = RigidBody2D.FREEZE_MODE_STATIC
 		if get_node("../SootSprite/SootSpriteImage").flip_h == false:
 			apply_impulse(Vector2(), Vector2(150, - 200)) 
 		else:
